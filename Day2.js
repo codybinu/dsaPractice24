d9b49defa9689a35c491e76a88c74f96eede14fc
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-08 22:52:10 +0530

    day 2 final

--- Day2.js
+++ Day2.js
@@ -29,3 +29,30 @@ function isPowerOfTwoBitWise(n){
 
 
 //given a no. N , find the nth elemenet of the fibonacci sequence;
+//TC: O(2^n)
+function fibonacci(n){
+    if(n===0){
+        return 0
+    }
+    if(n==1){
+        return 1;
+    }
+    let sp = fibonacci(n-1);
+    let ans = sp + fibonacci(n-2);
+    return ans;
+}
+
+// console.log(fibonacci(7))
+
+
+// Given an array of 'n' elements and a target element 't',
+//  find the index of 't' in the array. Return -1 if the target element is not found.
+//TC: O(N)
+function findTarget(t, arr){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] === t){   return i }
+    }
+     return -1 
+}
+
+console.log(findTarget(72,[1,8,4,2,3,7,4,3,4]))
